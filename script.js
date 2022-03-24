229d4ef94fb151aa3fca43dde3f6670b6e5c2280
Igor Sampaio <igorsamf@gmail.com>
2022-03-23 21:28:13 -0300

    tentativa do requisito 7

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 
 
 
-let ol = document.querySelector('#lista-tarefas')
+const listaTarefas = document.getElementById('lista-tarefas');
 let input = document.querySelector('#texto-tarefa');
 let btn = document.getElementById("criar-tarefa");
-
+let listCreated = document.getElementsByTagName('li');
 
 
 function recebeClick() {
     let list = document.createElement('li');
     list.innerText = input.value;
-    ol.appendChild(list);
+    listaTarefas.appendChild(list);
     input.value = '';
 }
 
 btn.addEventListener("click", recebeClick);
+
+function turnGray(event) {
+    for (let i = 0; i < listCreated.length; i =+ 1) {
+        listCreated[i].style.backgroundColor = '';
+    }
+    event.target.style.backgroundColor = 'gray';
+}
+listaTarefas.addEventListener('click', turnGray);
